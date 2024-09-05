2125badfc24f9e161eb65518ae1202da93d8bb29
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-05 22:42:44 +0530

    Added: Welcome Message is added

---
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,10 @@
+package com.moodanalyser;
+
+public class MoodAnalyser {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Mood Analyser Program");
+
+    }
+}
