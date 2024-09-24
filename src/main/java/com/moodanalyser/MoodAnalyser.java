b10cfbfc263eed8da28d0d0c4eb68fb1af34a0b3
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-24 18:08:59 +0530

    Added: Using Enum Throws Exception

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -8,15 +8,24 @@ public class MoodAnalyser {
     public MoodAnalyser(){
 
     }
-    public MoodAnalyser(String message){
+    public MoodAnalyser(String message) throws MoodAnalyzerException{
+        this.message = message;
+
+    }
+
+    public void analyzeMood(String msg) throws MoodAnalyzerException{
         this.message = message;
         analyseMood();
     }
 
     // Method to analyze the mood based on the message
-    public String analyseMood()
+    public String analyseMood() throws MoodAnalyzerException
     {
         try {
+
+            if (message.isEmpty()){
+                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.EMPTY, "Enter Input");
+            }
 
             if (message.contains("Sad")) {
                 return "SAD";
--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -1,5 +1,6 @@
 package com.moodanalyser;
 
+import org.junit.jupiter.api.Assertions;
 import org.testng.annotations.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MoodAnalyserTest {
 
     @Test
-    public void givenMessage_WhenProper_ShouldReturnSad(){
+    public void givenMessage_WhenProper_ShouldReturnSad() throws MoodAnalyzerException{
         MoodAnalyser moodAnalyser = new MoodAnalyser("I Am In Sad mood.");
         String result = moodAnalyser.analyseMood();
         System.out.println(result);
@@ -15,7 +16,7 @@ public class MoodAnalyserTest {
     }
 
     @Test
-    public void givenMessage_WhenProper_ShouldReturnHappy(){
+    public void givenMessage_WhenProper_ShouldReturnHappy() throws MoodAnalyzerException{
         MoodAnalyser moodAnalyser = new MoodAnalyser("I Am In Happy mood.");
         String result = moodAnalyser.analyseMood();
         System.out.println(result);
@@ -23,11 +24,26 @@ public class MoodAnalyserTest {
     }
 
     @Test
-    public void givenMessage_WhenNull_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String result = moodAnalyser.analyseMood();
-        System.out.println(result);
-        assertEquals("HAPPY", result);
+    void analyzeNullMood() throws MoodAnalyzerException {
+        MoodAnalyser moodAnalyzer3 = new MoodAnalyser(null);
+
+        try {
+            moodAnalyzer3.analyzeMood(null);
+        } catch (MoodAnalyzerException e) {
+            Assertions.assertEquals(MoodAnalyzerException.ExceptionType.NULL,e.type);
+        }
     }
+
+    @Test
+    void analyzeEmptyMood() throws MoodAnalyzerException {
+        MoodAnalyser moodAnalyzer4 = new MoodAnalyser(" ");
+
+        try {
+            moodAnalyzer4.analyzeMood(" ");
+        } catch (MoodAnalyzerException e) {
+            Assertions.assertEquals(MoodAnalyzerException.ExceptionType.EMPTY,e.type);
+        }
+    }
+
 
 }
